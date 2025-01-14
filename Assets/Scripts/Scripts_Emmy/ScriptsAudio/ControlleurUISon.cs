1c4aafacf2e6e9029eccd865bfeafd1a6fab1e73
EmmyFortin <fortin.emmy@gmail.com>
2025-01-13 19:32:29 -0500

    Fini

---
+++ Assets/Scripts/Scripts_Emmy/ScriptsAudio/ControlleurUISon.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
+
+public class ControlleurUISon : MonoBehaviour
+{
+    [SerializeField] private GameObject _panneauSon;
+
+
+
+
+    // Cette méthode est appelée lors d'un événement d'entrée
+    public void OnMenu(InputValue value)
+    {
+        Debug.Log("OnMenu appelé");
+        
+        // Si le panneau est déjà actif, on le désactive
+        if (_panneauSon.activeSelf)
+        {
+             Debug.Log("OnMenu désactive");
+            _panneauSon.SetActive(false);
+        }
+        else
+        {
+            // Sinon on l'active
+             Debug.Log("OnMenu active");
+            _panneauSon.SetActive(true);
+        }
+      
+
+    }
+
+    // // Pour fermer le menu
+    public void FermerMenu()
+    {
+        if (_panneauSon.activeSelf)
+        {
+            _panneauSon.SetActive(false);
+        }
+    }
+}
